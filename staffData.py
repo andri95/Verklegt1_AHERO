724bb711b3ef7604337e48f100674027e5d71d66
runarlevi <runar18@ru.is>
2019-12-02 14:49:07 +0000

    staffData - Rúnar

--- staffData.py
+++ staffData.py
@@ -1,5 +1,10 @@
 
 
 class staffData():
-    def __init__(self, name):
-        self.name = name
+    def __init__(self, Name, SSD, Address, cellPhone, phoneNumber, Email):
+        self.Name = Name
+        self.SSD = SSD
+        self.Address = Address
+        self.cellPhone = cellPhone
+        self.phoneNumber = phoneNumber
+        self.Email = Email
