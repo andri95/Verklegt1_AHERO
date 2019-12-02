d2d8122de9c5e4c44bcbf74f38dfb28675d185f2
Egill Smári Snorrasons <egill98@hotmail.com>
2019-12-02 15:11:28 +0000

    Airplane&Destination Data - Egill

--- airplaneData.py
+++ airplaneData.py
@@ -1,7 +1,13 @@
 
 class AirplaneData():
-    def __init__(self, Id, Type, Manufacturer, Capacity):
-        self.Id = Id
-        self.Type = Type
-        self.Manufacturer = Manufacturer
-        self.Capacity = Capacity
+    def __init__(self, id, type, manufacturer, capacity):
+        self.id = id
+        self.type = type
+        self.manufacturer = manufacturer
+        self.capacity = capacity
+    def __str__(self):
+        return "".format(self.id, self.type, self.type, self.manufacturer, self.capacity)
+
+
+newAirplane = AirplaneData("NABAE146", "F28", "BAE", 82 )
+print(newAirplane)
