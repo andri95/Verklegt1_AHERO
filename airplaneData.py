9558c7ca354a50bdef65f92a5817c01b348b69b5
Egill Smári Snorrasons <egill98@hotmail.com>
2019-12-02 14:57:44 +0000

    Airplane&Destination Data - Egill

---
+++ airplaneData.py
@@ -0,0 +1,7 @@
+
+class AirplaneData():
+    def __init__(self, Id, Type, Manufacturer, Capacity):
+        self.Id = Id
+        self.Type = Type
+        self.Manufacturer = Manufacturer
+        self.Capacity = Capacity
