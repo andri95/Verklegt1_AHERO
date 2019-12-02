9e67358bb2ea83b880ca059329c9360d2a9ad9a4
runarlevi <runar18@ru.is>
2019-12-02 15:33:43 +0000

    staffData - Rúnar

--- staffData.py
+++ staffData.py
@@ -1,10 +1,17 @@
 
 
 class staffData():
-    def __init__(self, name, ssn, address, cellphone, phoneNumber, email):
+    def __init__(self, SSN, name, role, rank, license="N/A"):
+        self.SSN = SSN
         self.name = name
-        self.ssn = ssn
-        self.address = address
-        self.cellphone = cellphone
-        self.phoneNumber = phoneNumber
-        self.email = email
+        self.role = role
+        self.rank = rank
+        self.license = license
+
+    def __str__(self):
+        return "ssn: {}\nname: {}\nrole: {}\nrank: {}\nlicence: {}".format(self.SSN, self.name, self.role, self.rank,
+                                                                           self.license)
+
+
+staff1 = staffData("1611983239", "Rúnar", "Pilot", "Captain")
+print(staff1)
