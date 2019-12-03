65fd3ee9bfdae6e293323b93f42568ffc270a699
Andri Pétursson <andripetursson@Andris-MacBook-Pro-4.local>
2019-12-03 16:34:46 +0000

    updated

--- updateIO.py
+++ updateIO.py
@@ -9,25 +9,35 @@ class UpdateIO:
 
     def updateDestIO(self, country, contact, emergencyNumber):
 
-        with open(self.destPath, 'w+') as destinationsFile:
+        with open(self.destPath, 'r') as destinationsFile:
             reader = csv.DictReader(destinationsFile)
             destinations_list = []
 
             for row in reader:
                 destinations_list.append(DestinationData(row['country'], row['flightTime'], row['contact'], row['emergencyNumber']))
 
+        with open(self.destPath, 'w') as destinationsFile:
+            fieldnames = ['country','flightTime','contact','emergencyNumber']
+            writer = csv.DictWriter(destinationsFile, fieldnames=fieldnames)
+            writer.writeheader()
+
+        with open(self.destPath, 'a') as destinationsFile:
             fieldnames = ['country','flightTime','contact','emergencyNumber']
             writer = csv.DictWriter(destinationsFile, fieldnames=fieldnames)
             for destination in destinations_list:
                 if destination.getCountry() == country:
                     destination.setContact(contact)
-                    destination.emergencyNumber(emergencyNumber)
-                writer.writerow({'country': destination.getCountry(), 'flightTime': destination.getFlightTime(),
+                    destination.setEmergencyNumber(emergencyNumber)
+                writer.writerow({'country': destination.getCountry(), 'flightTime': destination.getFlightTime(), 
                                 'contact': destination.getContact(), 'emergencyNumber': destination.getEmergencyNumber()})
+
+    def addLicenseIO(self, SSN):
+
+        
 
 
             
 
 test = UpdateIO()
 
-test.updateDestIO('Thorshavn', 'Darth', '6666969')
+test.updateDestIO('Thorshavn', 'Vader', '6666969')
