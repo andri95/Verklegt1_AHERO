ad4c8453d6be13506e5c00c6aca0f00d1857f7cb
Andri Pétursson <andripetursson@Andris-MacBook-Pro-4.local>
2019-12-03 14:06:44 +0000

    Merge

--- flightData.py
+++ flightData.py
@@ -8,5 +8,23 @@ class FlightData:
         self.flightAttentants = flightAttentants
         self.flightID = flightID
 
+    def getDestination(self):
+        return self.destination
+
+    def getDate(self):
+        return self.date
+
+    def getTime(self):
+        return self.time
+
+    def getPilots(self):
+        return self.pilots
+
+    def getFlightAttendants(self):
+        return self.flightAttentants
+
+    def getFlightID(self):
+        return self.flightID
+
     def __str__(self):
         return 'Destination: {}\nDate: {}\nTime: {}\nPilots: {}\nFlight Attentants: {}\nVoyage ID: {}'.format(self.destination, self.date, self.time, '\n'.join([pilot for pilot in self.pilots]), '\n'.join([fa for fa in self.flightAttentants]), self.flightID)
--- staffData.py
+++ staffData.py
@@ -1,6 +1,5 @@
 
-
-class staffData():
+class StaffData():
     def __init__(self, SSN, name, role, rank, license="N/A"):
         self.SSN = SSN
         self.name = name
@@ -8,9 +7,22 @@ class staffData():
         self.rank = rank
         self.license = license
 
+    def getSSN(self):
+        return self.SSN
+
+    def getName(self):
+        return self.name
+
+    def getRole(self):
+        return self.role
+
+    def getRank(self):
+        return self.rank
+
+    def getLicence(self):
+        return self.license
+
     def __str__(self):
         return "ssn: {}\nname: {}\nrole: {}\nrank: {}\nlicence: {}".format(self.SSN, self.name, self.role, self.rank, self.license)
 
 
-staff1 = staffData("1611983239", "Rúnar", "Pilot", "Captain")
-print(staff1)
