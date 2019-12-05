7a9840ceab4bbed27bda07c7ad4e4a519f94ce4f
Andri Pétursson <andripetursson@Andris-MacBook-Pro-4.local>
2019-12-05 22:49:07 +0000

    employee ui update

--- UI/EmployeeUI.py
+++ UI/EmployeeUI.py
@@ -76,26 +76,16 @@ class EmployeeMenu():
             user_input = input("Input a command: ")
 
             if user_input == "1":
-                staffObject_list = self.mainObject.getAllStaffLL()
-                for staffMember in staffObject_list:
-                    print('Name: {}, Role: {}'.format(staffMember.getName(), staffMember.getRole()))
+                self.getAllStaffUI()
 
             elif user_input == "2":
-                pilotObject_list = self.mainObject.getAllPilotsLL()
-                for staffMember in pilotObject_list:
-                    print('Name: {}, Role: {}'.format(staffMember.getName(), staffMember.getRole()))
+                self.getAllPilotsUI()
 
             elif user_input == "3":
-                cabinCrewObject_list = self.mainObject.getAllCabinCrewLL()
-                for staffMember in cabinCrewObject_list:
-                    print('Name: {}, Role: {}'.format(staffMember.getName(), staffMember.getRole()))
+                self.getAllCabinCrewUI()
 
             elif user_input == "4":
-                staffObject_list = self.mainObject.getAllStaffLL()
-                for staffMember in staffObject_list:
-                    print('Name: {}, SSN: {}'.format(staffMember.getName(), staffMember.getSSN()))
-                input_ssn = input("Enter social security number: ")
-                self.getStaffByID(input_ssn)   
+                self.getStaffByID()   
 
             elif user_input == "q":
                 Goodbye()
@@ -105,6 +95,21 @@ class EmployeeMenu():
 
             else:
                 print("Invalid command")
+
+    def getAllStaffUI(self):
+        staffObject_list = self.mainObject.getAllStaffLL()
+        for staffMember in staffObject_list:
+            print('Name: {}, Role: {}'.format(staffMember.getName(), staffMember.getRole()))
+
+    def getAllPilotsUI(self):
+        pilotObject_list = self.mainObject.getAllPilotsLL()
+        for staffMember in pilotObject_list:
+            print('Name: {}, Role: {}'.format(staffMember.getName(), staffMember.getRole()))
+
+    def getAllCabinCrewUI(self):
+        cabinCrewObject_list = self.mainObject.getAllCabinCrewLL()
+        for staffMember in cabinCrewObject_list:
+            print('Name: {}, Role: {}'.format(staffMember.getName(), staffMember.getRole()))
 
     def addNewStaffUI(self):
 
@@ -120,8 +125,12 @@ class EmployeeMenu():
         newEmployee = StaffData(ssn, name, address, cellPhone, phoneNumber, email, role, rank, license_str)
         self.mainObject.addNewStaffLL(newEmployee)
 
-    def getStaffByID(self, ssn):
-        staffMember = self.mainObject.getStaffByIDLL(ssn)
+    def getStaffByID(self):
+        staffObject_list = self.mainObject.getAllStaffLL()
+        for staffMember in staffObject_list:
+            print('Name: {}, SSN: {}'.format(staffMember.getName(), staffMember.getSSN()))
+        input_ssn = input("Enter social security number: ")
+        staffMember = self.mainObject.getStaffByIDLL(input_ssn)
         print(staffMember)
 
 
